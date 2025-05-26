c3d7b450b3f80366fe97b2bee9c5f5cb4490abba
Your Name <your_email@example.com>
2025-05-26 01:31:54 -0400

    added URL output to outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+# print the Lambda URL
+
+output "api_gateway_url" {
+  value = "https://${aws_api_gateway_rest_api.crm_api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}/contacts"
+}
