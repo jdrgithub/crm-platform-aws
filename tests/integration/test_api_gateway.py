5486e392edca87871d36644e94cfa67bc48786de
Your Name <your_email@example.com>
2025-05-26 19:17:32 -0400

    blah

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -1,11 +1,17 @@
 import os
-import requests
-import pytest
 from dotenv import load_dotenv
 
-@pytest.mark.skipif(not os.getenv("API_GATEWAY_URL"), reason="API_GATEWAY_URL not set")
+load_dotenv()
+
+import requests
+import pytest
+
+@pytest.mark.skipif(
+    not os.getenv("API_GATEWAY_URL"),
+    reason="API_GATEWAY_URL not set"
+)
 def test_post_contacts():
-    url = os.getenv("API_GATEWAY_URL") # lOAD FROM .env or Jenkins env
-    response = requests.post(f"{url}")
+    url = os.getenv("API_GATEWAY_URL")
+    response = requests.post(url)
     assert response.status_code == 200
     assert "Hello from CRM Lambda" in response.text
