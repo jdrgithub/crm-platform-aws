0c517130e7622d8ffdca2813484c923af298f106
Your Name <your_email@example.com>
2025-05-26 01:04:20 -0400

    added initial CRM lambda function

---
+++ terraform/lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "crm_handler" {
+    function_name   = "${var.project_name}-handler"
+    role            = aws_iam_role.lambda_exec_role.arn
+    handler         = "main.handler"
+    runtime         = "python3.12"
+
+    filename        = "${path.module}/lambda/lambda_function.zip"
+    source_code_hash = filebase64sha256("${path.module}/lambda/lambda_function.zip")
+
+    environment {
+        variables = {
+            DYNAMODB_TABLE = aws_dynamodb_table.crm_contacts.name
+        }
+    }
+
+    tags = {
+        Name        = "CRM Lambda Handler"
+        Environment = var.environment
+    }
+}
