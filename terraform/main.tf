ef28d3291f6c4c64b1ea8d9a3e3147edb8ecc4dc
Your Name <your_email@example.com>
2025-05-25 23:21:48 -0400

    added tfstate to s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
 # main.tf
 
 terraform {
+    backend "s3" {
+        bucket      = "crm-platform-data-bucket"
+        key         = "terraform/state/terraform.tfstate"
+        region      = "us-east-1"
+        encrypt     = true
+    }
+    
     required_providers {
         aws = {
             source = "hashicorp/aws"
