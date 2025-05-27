f6229373a97ab49b848611c80d0676c02778c9f0
Your Name <your_email@example.com>
2025-05-27 00:35:36 -0400

    blah

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Explicitly load the .env from project root
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../.env'))
 
 import requests
 import pytest
