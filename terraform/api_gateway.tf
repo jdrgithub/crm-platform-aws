1445b1c8dba3c187151d8130b77d24e669d56d2a
Your Name <your_email@example.com>
2025-05-28 08:39:14 -0400

    capitalized POST

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -51,7 +51,7 @@ resource "aws_api_gateway_integration" "lambda_get" {
     rest_api_id             = aws_api_gateway_rest_api.crm_api.id
     resource_id             = aws_api_gateway_resource.contacts.id
     http_method             = aws_api_gateway_method.get_contacts.http_method
-    integration_http_method = POST
+    integration_http_method = "POST"
     type                    = "AWS_PROXY"
     uri                     = aws_lambda_function.get_contacts.invoke_arn
 
