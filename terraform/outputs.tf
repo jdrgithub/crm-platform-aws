3e64caee3fe8bdcdfdd9384b404c01153d8fec20
Your Name <your_email@example.com>
2025-05-28 23:09:48 -0400

    added bucket url to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "api_gateway_url" {
 output "frontend_bucket" {
   value = aws_s3_bucket.frontend.id
 }
+
+output "frontend_bucket_url" {
+  value = "https://${aws_s3_bucket.frontend.bucket}.s3.amazonaws.com"
+  description = "Public URL of the frontend S3 bucket"
+}
