aff12ecd9da368e09f755947147fbc0a949afefa
Your Name <your_email@example.com>
2025-05-29 17:57:56 -0400

    trying static policy with here doc

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -9,27 +9,26 @@ resource "aws_cognito_identity_pool" "demo_pool" {
 resource "aws_iam_role" "unauthenticated_role" {
   name = "crm-demo-unauth-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Federated = "cognito-identity.amazonaws.com"
-      },
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        # aud -> Audience -> intended recipient of a token
-        # Ensures only tokens issued for id pool can assume role
-        StringEquals = {
-          "cognito-identity.amazonaws.com:aud" = aws_cognito_identity_pool.demo_pool.id
-        }
-        # amr -> Authentication Method Reference -> how identity authed 
-        # Filters based on how id is authed -> unauthed = guest users
-        # stringlike allows multiple values 
-        "ForAnyValue:StringLike" = {
-          "cognito-identity.amazonaws.com:amr" = "unauthenticated"
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "cognito-identity.amazonaws.com"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "cognito-identity.amazonaws.com:aud": "${aws_cognito_identity_pool.demo_pool.id}"
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "unauthenticated"
+          }
         }
       }
-    }]
-  })
+    ]
+  }
+  POLICY
 }
