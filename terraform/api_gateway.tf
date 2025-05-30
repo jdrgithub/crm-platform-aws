96b2e3810e24ba354b63808adb92caf142f97baf
Your Name <your_email@example.com>
2025-05-30 01:49:25 -0400

    removed mock delete added real one

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -83,22 +83,15 @@ resource "aws_api_gateway_method" "delete_contact" {
   authorization = "NONE"
 }
 
-# Adding mock integration to satisfy preflight check
-resource "aws_api_gateway_integration" "delete_mock" {
+resource "aws_api_gateway_integration" "lambda_delete" {
   rest_api_id             = aws_api_gateway_rest_api.crm_api.id
-  resource_id = aws_api_gateway_resource.contact_id.id
-  http_method             = aws_api_gateway_method.delete_contact.http_method
-  type                    = "MOCK"
-  integration_http_method = "DELETE"
+  resource_id             = aws_api_gateway_resource.contact_id.id
+  http_method             = "DELETE"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.crm_handler.invoke_arn
+}
 
-  request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
-}
-EOF
-  }
-}
 
 resource "aws_api_gateway_method_response" "delete_200" {
   rest_api_id = aws_api_gateway_rest_api.crm_api.id
@@ -255,7 +248,8 @@ resource "aws_api_gateway_deployment" "crm_api_deploy" {
     aws_api_gateway_integration.lambda_get,
     aws_api_gateway_integration.options_contacts,
     aws_api_gateway_integration.options_contact_id,
-    aws_api_gateway_method.options_contact_id          
+    aws_api_gateway_method.options_contact_id,          
+    aws_api_gateway_integration.lambda_delete
   ]
   rest_api_id = aws_api_gateway_rest_api.crm_api.id
 
