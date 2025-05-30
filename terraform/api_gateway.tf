42c1f6f509f289186c2d1407af38df0d09cfc6c6
Your Name <your_email@example.com>
2025-05-29 23:40:51 -0400

    split off integration response

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -43,21 +43,25 @@ resource "aws_api_gateway_integration" "options_contacts" {
 }
 EOF
   }
+}
 
-  integration_response {
-    status_code = "200"
+resource "aws_api_gateway_integration_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.crm_api.id
+  resource_id = aws_api_gateway_resource.contacts.id
+  http_method = aws_api_gateway_method.options_contacts.http_method
+  status_code = "200"
 
-    response_parameters = {
-      "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-      "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS,DELETE'",
-      "method.response.header.Access-Control-Allow-Origin"  = "'${var.frontend_origin}'"
-    }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'${var.frontend_origin}'"
+  }
 
-    response_templates = {
-      "application/json" = ""
-    }
+  response_templates = {
+    "application/json" = ""
   }
 }
+
 
 resource "aws_api_gateway_method" "post_contact" {
   rest_api_id   = aws_api_gateway_rest_api.crm_api.id
