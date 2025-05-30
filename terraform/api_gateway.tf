699f75d7583461b329d984930e6af179fe128d30
Your Name <your_email@example.com>
2025-05-30 01:59:23 -0400

    added depends on to delete_200

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -94,6 +94,7 @@ resource "aws_api_gateway_integration" "lambda_delete" {
 
 
 resource "aws_api_gateway_method_response" "delete_200" {
+  depends_on  = [aws_api_gateway_integration.lambda_delete]
   rest_api_id = aws_api_gateway_rest_api.crm_api.id
   resource_id = aws_api_gateway_resource.contact_id.id
   http_method = aws_api_gateway_method.delete_contact.http_method
